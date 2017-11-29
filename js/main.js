241615dbbeef11709c13b5b1e5a9c6397cd1d956
BillCurry <bill.curry444@gmail.com>
2017-11-29 10:40:28 -0500

    v2using hideseek search
    I've added the hideseek search plug-in to my index.html but would still like to write my own search code, which is in main.js

--- js/main.js
+++ js/main.js
@@ -1,28 +1,4 @@
-
-
-
-// function myFunction() {
-//     // Declare variables
-//     var caption, input, filter, div, a;
-//     caption = document.getElementsByTagName("a")[0].getAttribute("data-title");
-//     input = document.getElementById('myInput');
-//     filter = input.value.toLowerCase();
-//     div = document.getElementByClass('image-container');
-//     a = document.getElementsByTagName("a");
-
-//     // Loop through all a items, and hide those who don't match the search query
-//     for (var i = 0; i < a.length; i++) {
-//         a = .image-container[i].getElementsByTagName("a")[0];
-//         if (a.innerHTML.toLowerCase().indexOf(filter) > -1) {
-//             a[i].style.display = "";
-//         } else {
-//             a[i].style.display = "none";
-//         }
-//     }
-// }
-
-
-  function myFunction() {
+function myFunction() {
     // Declare variables
     var caption, input, filter, div, a;
     input = document.getElementById('myInput');
@@ -33,7 +9,7 @@
     // Loop through all a items, and hide those who don't match the search query
     for (var i = 0; i < a.length; i++) {
         caption = document.getElementsByTagName("a")[0].getAttribute("data-title");
-        if (caption.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        if (caption.toLowerCase().indexOf(filter) > -1) {
             caption[i].style.display = "";
         } else {
             caption[i].style.display = "none";
